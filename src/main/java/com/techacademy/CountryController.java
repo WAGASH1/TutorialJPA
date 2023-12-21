3ff480f0c2bbd0064b139509cc88523ec7eacc70
tomo <takuannshoui@icloud.com>
2023-12-21 14:09:26 +0900

    Lesson16 課題

--- src/main/java/com/techacademy/CountryController.java
+++ src/main/java/com/techacademy/CountryController.java
@@ -18,7 +18,7 @@ public class CountryController {
     }
 
     // ----- 一覧画面 -----
-    @GetMapping("/list")
+    @GetMapping(value = { "/list", "/country/list"})
     public String getList(Model model) {
         // 全件検索結果をModelに登録
         model.addAttribute("countrylist", service.getCountryList());
@@ -27,31 +27,37 @@ public class CountryController {
     }
 
     //詳細画面
-    @GetMapping(value = {"/detail", "/detail/{code}/" })
+    @GetMapping(value = {"/detail", "/detail/{code}/"})
     public String getCountry(@PathVariable(name = "code" , required = false)String code, Model model) {
+        //codeが指定されていたら検索結果、なければ空のクラスを設定
         Country country = code != null ? service.getCountry(code) : new Country();
+        //Modelに登録
         model.addAttribute("country", country);
+        // 画面遷移
         return "country/detail";
     }
-
+    //更新（追加）
     @PostMapping("/detail")
     public String postCountry(@RequestParam("code") String code, @RequestParam("name") String name,
             @RequestParam("population") int population, Model model) {
+        //更新（追加）
         service.updateCountry(code, name, population);
-
+        //一覧画面にリダイレクト
         return "redirect:/country/list";
-
     }
 
-    @GetMapping("/delete")
-    public String deleteCountryForm(Model model) {
+    //削除画面
+    @GetMapping(value = {"/delete","delete/{code}"})
+    public String deleteCountryForm(@PathVariable(required = false) String code, Model model) {
+        model.addAttribute("code", code);
         return "country/delete";
     }
 
+    //削除
     @PostMapping("/delete")
     public String deleteCountry(@RequestParam("code") String code, Model model) {
         service.deleteCountry(code);
-
+    //一覧画面にリダイレクト
         return "redirect:/country/list";
     }
 }
